0f4dbf4f18eb971c5289592cdb6ebd67840aefc5
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-28 16:50:25 +0000

    keeping views clean

--- app.rb
+++ app.rb
@@ -2,12 +2,7 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/' do
-  "Hello World"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
