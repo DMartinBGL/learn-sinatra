7ccf3b976c9f88342873a12a5027da72409a285a
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-29 14:20:38 +0000

    params

---
+++ battle/app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
+  erb(:index)
+end
